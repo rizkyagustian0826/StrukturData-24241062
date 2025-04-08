befec00ad12830c4f5370120a890fb10712d0083
rizkyagustian0826 <rizkyagustian049@gmail.com>
2025-04-08 19:59:48 +0800

    Add files via upload

---
+++ PRE-1(PERULANGAN)/foor-loop.py
@@ -0,0 +1,42 @@
+# perulangan (loop)
+
+# for kondisi: jika kondisi terpenuhi
+#     aksi JALANKAN aksi
+
+angka_list = [0,2,4,8,10] # ini adalah list 
+print(angka_list)
+
+# angka = 0
+# print(angka)
+# angka += 1 # angka = angka +1
+# print (angka)
+# angka += 1
+# print (angka)
+
+# for-loop
+for i in angka_list:
+    print(f"i yang sekarang -> {i}")
+
+print("loop berakhir \n")    
+
+
+# perulangan (loop)
+
+# for kondisi: jika kondisi terpenuhi
+#     aksi JALANKAN aksi
+
+range = [0,1,2,3,4] # ini adalah range
+print(range)
+
+# angka = 0
+# print(angka)
+# angka += 1 # angka = angka +1
+# print (angka)
+# angka += 1
+# print (angka)
+
+# for-loop
+for i in range:
+    print(f"i yang sekarang -> {i}")
+
+print("loop berakhir \n")
